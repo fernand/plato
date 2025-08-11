75b6ac781ce46e0f5b1952a0cfece01b9d5848c1
Fernand Pajot <fernand@epigram.me>
2025-08-11 15:12:54 -0600

    .

--- tinystories_data.py
+++ tinystories_data.py
@@ -1,29 +1,30 @@
 import datasets
 import transformers
 
-dataset = datasets.load_dataset('roneneldan/TinyStories')
-# tokenizer = transformers.GPTNeoXTokenizerFast.from_pretrained('EleutherAI/gpt-neox-20b')
-tokenizer = transformers.PreTrainedTokenizerFast(
-        tokenizer_file="plato_tokenizer.json",
-        bos_token="<s>",
-        eos_token="</s>",
-        pad_token="<pad>",
-        unk_token="<unk>",
-        mask_token="<mask>"
+if __name__ == '__main__':
+    dataset = datasets.load_dataset('roneneldan/TinyStories')
+    # tokenizer = transformers.GPTNeoXTokenizerFast.from_pretrained('EleutherAI/gpt-neox-20b')
+    tokenizer = transformers.PreTrainedTokenizerFast(
+            tokenizer_file="plato_tokenizer.json",
+            bos_token="<s>",
+            eos_token="</s>",
+            pad_token="<pad>",
+            unk_token="<unk>",
+            mask_token="<mask>"
+        )
+
+    def tokenize_with_eos(batch):
+        return tokenizer(
+            [text + tokenizer.eos_token for text in batch['text']],
+            truncation=False,
+            padding=False
+        )
+
+    tokenized_dataset = dataset.map(
+        tokenize_with_eos,
+        batched=True,
+        remove_columns=dataset['train'].column_names,
+        num_proc=32,
     )
 
-def tokenize_with_eos(batch):
-    return tokenizer(
-        [text + tokenizer.eos_token for text in batch['text']],
-        truncation=False,
-        padding=False
-    )
-
-tokenized_dataset = dataset.map(
-    tokenize_with_eos,
-    batched=True,
-    remove_columns=dataset['train'].column_names,
-    num_proc=32,
-)
-
-tokenized_dataset.save_to_disk('tokenized_tinystories_plato')
+    tokenized_dataset.save_to_disk('tokenized_tinystories_plato')
